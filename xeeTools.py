25c2e662e774c0778dd527f65d71c7c044ecee2d
xee8ai <xee8ai@users.noreply.github.com>
2024-03-20 10:45:33 +0100

    Apply changes by black.

--- xeeTools.py
+++ xeeTools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-'''A module holding some functions frequently used by me.'''
+"""A module holding some functions frequently used by me."""
 
 from pprint import pprint
 import inspect
@@ -10,44 +10,44 @@ import traceback
 
 ################################################################################
 def dd(*data):
-    ''' Helper for fast debug view of each kind of data.
-    Inspirated by Laravel's dd() function.'''
+    """Helper for fast debug view of each kind of data.
+    Inspirated by Laravel's dd() function."""
 
     print()
-    print(100 * '=')
+    print(100 * "=")
     # get the caller's metadata: class and method
     print()
-    print('Output of dd(), ', end='')
+    print("Output of dd(), ", end="")
 
     # check if in class context
     stack = inspect.stack()
-    if 'self' in stack[1][0].f_locals:
+    if "self" in stack[1][0].f_locals:
         the_class = str(stack[1][0].f_locals["self"].__class__)
         the_class = the_class.split("'")[1]
         the_method = stack[1][0].f_code.co_name
-        print('called by:')
-        print('{}.{}()'.format(str(the_class), the_method))
+        print("called by:")
+        print("{}.{}()".format(str(the_class), the_method))
     else:
-        print('called from')
+        print("called from")
 
     # get the caller's metadata: file and line
     tb = traceback.extract_stack(limit=2)
-    print('{}, line {}'.format(tb[0][0], tb[0][1]))
+    print("{}, line {}".format(tb[0][0], tb[0][1]))
     print()
 
     # pretty print all given data
     for i, d in enumerate(data):
         print()
-        print(80 * '=')
+        print(80 * "=")
         print()
-        the_type = 'Argument {} is of type {}'.format(i, type(d))
+        the_type = "Argument {} is of type {}".format(i, type(d))
         print(the_type)
-        print(len(the_type) * '-')
+        print(len(the_type) * "-")
         pprint(d)
         print()
 
     print()
-    print(100 * '=')
+    print(100 * "=")
     print()
     print()
 
@@ -57,12 +57,12 @@ def dd(*data):
 
 ################################################################################
 def ex_to_str(ex):
-    '''Converts an exception to a human readable string containing relevant informations.
-    Use e.g. to create meaningful log entries.'''
+    """Converts an exception to a human readable string containing relevant informations.
+    Use e.g. to create meaningful log entries."""
 
     # get type and message of risen exception
-    ex_type = '{}'.format(type(ex)).split("'")[1]
-    ex_msg = '{}'.format(ex)
+    ex_type = "{}".format(type(ex)).split("'")[1]
+    ex_msg = "{}".format(ex)
 
     # get the command where the exception has been raised
     tb = traceback.extract_tb(sys.exc_info()[2], limit=2)
@@ -72,12 +72,8 @@ def ex_to_str(ex):
 
     # the string (one liner) to return
     nice_ex = '{} ({}) raised executing "{}" in {}, line {}'.format(
-            ex_type,
-            ex_msg,
-            ex_cmd,
-            ex_file,
-            ex_line
-            )
+        ex_type, ex_msg, ex_cmd, ex_file, ex_line
+    )
 
     return nice_ex
 
@@ -94,7 +90,7 @@ def seconds_to_timestring(seconds):
 ################################################################################
 ################################################################################
 ################################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # test the exception converter
     try:
@@ -106,5 +102,5 @@ if __name__ == '__main__':
         # raise
 
     # test dd last because it ends the script :-)
-    l = ['foo', 'bar', 42]
-    dd('test', l)
+    l = ["foo", "bar", 42]
+    dd("test", l)
